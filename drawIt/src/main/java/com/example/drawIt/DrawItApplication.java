17dfb0e53804c4007aa8287d3dae59491fd38cab
sgn08062 <sgn08062@gmail.com>
2025-12-12 14:16:55 +0900

    초안

---
+++ drawIt/src/main/java/com/example/drawIt/DrawItApplication.java
@@ -0,0 +1,13 @@
+package com.example.drawIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawItApplication.class, args);
+	}
+
+}
---
+++ drawIt/src/test/java/com/example/drawIt/DrawItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drawIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
