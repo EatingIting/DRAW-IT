688bde03e8ed8833b15bd9fb32e6b1a3fcba2550
EatingIting <euijae0611@naver.com>
2025-12-12 17:48:56 +0900

    방만들기 이후 백엔드 로직 구현(나중에 JPA 붙여야 할듯)

--- BackEnd/src/main/java/com/example/drawIt/Controller/LobbyController.java
+++ BackEnd/src/main/java/com/example/drawIt/Controller/LobbyController.java
@@ -4,8 +4,8 @@ import com.example.drawIt.DTO.CreateLobbyRequest;
 import com.example.drawIt.DTO.CreateLobbyResponse;
 import com.example.drawIt.Service.LobbyService;
 import com.example.drawIt.domain.Lobby;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
 public class LobbyController {
 
-    private LobbyService lobbyService;
+    private final LobbyService lobbyService;
 
     @PostMapping("/lobby")
     public ResponseEntity<CreateLobbyResponse> createLobby(@RequestBody CreateLobbyRequest request) {
